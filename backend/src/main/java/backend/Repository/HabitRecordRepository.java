5eec6136fda580febcb042708eb5186c7a944960
Ryan Guedes <ryanguedesbarradas@gmail.com>
2026-07-06 08:24:30 -0400

    feat: adiciona o método de busca por id do hábito e data

--- backend/src/main/java/backend/Repository/HabitRecordRepository.java
+++ backend/src/main/java/backend/Repository/HabitRecordRepository.java
@@ -1,7 +1,13 @@
-package com.ryan.minimalist_habit_tracker.Repository;
+package backend.Repository;
 
-import com.ryan.minimalist_habit_tracker.Model.HabitRecord;
+import backend.Model.HabitRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDate;
+import java.util.Optional;
 
 public interface HabitRecordRepository extends JpaRepository<HabitRecord, Long> {
+
+    Optional<HabitRecord> findByHabitIdAndDate(Long habitId, LocalDate date);
+
 }
+
