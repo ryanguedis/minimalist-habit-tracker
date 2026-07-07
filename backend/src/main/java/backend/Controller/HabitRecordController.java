bf1aeaea1f3e0d3eb57e3e0ddba21750c4aa6989
RYAN GUEDES BARRADAS <00798547294@seduc.am.gov.br>
2026-07-07 11:34:51 -0400

    feat: adiciona o HabitRecordController

--- backend/src/main/java/backend/Controller/HabitRecordController.java
+++ backend/src/main/java/backend/Controller/HabitRecordController.java
@@ -1,4 +1,35 @@
 package backend.Controller;
 
+import backend.Model.HabitRecord;
+import backend.Service.HabitRecordService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/records")
 public class HabitRecordController {
+
+    private final HabitRecordService service;
+
+    public HabitRecordController(HabitRecordService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<HabitRecord> createOrGet(@PathVariable Long habitId) {
+        HabitRecord createOrGet = service.createOrGet(habitId);
+        return ResponseEntity.ok(createOrGet);
+    }
+
+    @PatchMapping("/check")
+    public ResponseEntity<HabitRecord> check(@PathVariable Long id) {
+        HabitRecord recordChecked = service.check(id);
+        return ResponseEntity.ok(recordChecked);
+    }
+
+    @PatchMapping("/uncheck")
+    public ResponseEntity<HabitRecord> uncheck(@PathVariable Long id) {
+        HabitRecord recordUnchecked = service.uncheck(id);
+        return ResponseEntity.ok(recordUnchecked);
+    }
 }
