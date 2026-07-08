6ec1fe34be40a8d01c70985628b41b94835e2814
Ryan Guedes <ryanguedesbarradas@gmail.com>
2026-07-08 07:34:13 -0400

    feat: configurando e gerando primeira versão do db

---
+++ backend/src/main/resources/db/migration/V1__create_tables.sql
@@ -0,0 +1,15 @@
+CREATE TABLE tb_habit (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
+    habitStreak INT NOT NULL,
+    totalChecked BIGINT NOT NULL
+);
+
+CREATE TABLE tb_record (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    date DATE NOT NULL,
+    checked BOOLEAN NOT NULL,
+    habit_id BIGINT,
+    FOREIGN KEY (habit_id) REFERENCES tb_habit(id)
+);
